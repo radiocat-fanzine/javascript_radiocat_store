5b765851d7b93772efa68ba749285a3ef087e52a
Luzmila Quezada <radiocat.fanzine@gmail.com>
2025-08-11 19:23:31 -0500

    iteracion carrito y contador

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,83 @@
+const cartContent = document.getElementById("cartContent");
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+//Funcion para visualizar el items y total del carrito de compra, borrar productos y agregar cantidades
+
+//Funcion con paramentros 2 + evento
+function displayCart(shopList) {
+
+    cartContent.innerHTML = "";
+
+    let header = document.createElement("div");
+    header.className = "cardHeader"
+    header.innerHTML = `
+    <div class="left-info-header">
+            <h2>Your Purchase :</h2>
+        </div>
+        <div class="center-info-header">
+            <h2>Price :</h2>
+            <h2>Unit :</h2>
+            <h2>Total :</h2>
+        </div>
+        `;
+    cartContent.appendChild(header);
+
+    if (shopList.length === 0) {
+        let emptyCart = document.createElement("p");
+        emptyCart.className = "no-results-cart";
+        emptyCart.innerText = "Your cart is empty.";
+        cartContent.appendChild(emptyCart)
+        return;
+    }
+
+    shopList.forEach((cartProduct, index) => {  //Metodo .forEach para mostrar items del carrito
+        let content = document.createElement("div");
+        content.className = "cardItem";
+        content.innerHTML = `
+        <div class="left-info">
+            <img src="${cartProduct.img}" alt="${cartProduct.name}">
+            <h3>${cartProduct.name}</h3>
+        </div>
+        <div class="center-info">                  
+            <p class="price">€${cartProduct.price}</p>
+            <div class="unit-buttons">
+            <button class="counter">-</button>  
+            <p class="unit">${cartProduct.units}</p>
+            <button class="counter">+</button>
+            </div>
+            <h3 class="total">€${cartProduct.price*cartProduct.units}</h3>
+        </div>
+        `;
+        
+        cartContent.append(content);
+
+        let deleteProduct = document.createElement("button");
+        deleteProduct.innerText = "🗑";
+        deleteProduct.className = "deleteProduct";
+        content.append(deleteProduct);
+        
+        deleteProduct.addEventListener("click", () => { //Evento: Borrar Item del carrito, Metodo .splice
+            cart.splice(index, 1);
+            localStorage.setItem("cart", JSON.stringify(cart));
+            displayCart(cart);
+        });
+    });
+    const total = cart.reduce((acc, item) => acc + item.price*item.units, 0); //Metodo .reduce para sumar los precios y cantidades del carrito
+    
+    let footer = document.createElement("div");
+    footer.className = "cardFooter"
+    footer.innerHTML = `<h2>Your total is : €${total}</h2>`;
+    cartContent.appendChild(footer);
+};
+
+displayCart(cart);
+
+
+
+
+
+
 //Funcion aplicar descuento 50% en prints//
 
     function descuentoPrints(name, price) {
@@ -16,4 +96,4 @@
     
     }
 
-    descuentoPrints ("*Demon lover* Print", 15)
+    //descuentoPrints ()
--- js/main.js
+++ js/main.js
@@ -1,43 +1,102 @@
 const shopContent = document.getElementById("shopContent");
 const searchBar = document.getElementById("searchBar");
-const noResults = document.getElementById("noResults")
+const noResults = document.getElementById("noResults");
+const cartCounter = document.getElementById("cartCounter")
 
-//Fetch y Funcion para mostrar productos de la tienda con if y else
 
-let products = [];
+//Funciones para obtener los productos desde JSON y mostrarlos en la tienda
 
-async function getProducts () {
-    const response = await fetch("./db/data.json");
+let products = []; // Array para colocar los objetos de JSON
+
+//Funcion asincrona de orden superior 1 para obtener los productos
+async function getProducts () {       
+    const response = await fetch("./db/data.json"); // Fetch / JSON
     const data = await response.json();
     products = data;
-    console.log(data)
     displayProducts(data)
     }
 
+let cart = []; // Array para colocar los productos comprados
+
+//Funcion 1 para contador de productos en nav bar
+
+function countingCart() {
+    const currentCart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    if (countCart) {
+        if (currentCart.length === 0) {
+        countCart.style.display = "none";
+        } else {
+            countCart.style.display = "block";
+            const totalUnits = currentCart.reduce((acc, item) => acc + (item.units || 1), 0);
+            countCart.innerText = totalUnits;
+        };
+    };
+};
+
+countingCart();
+
+
+//Funcion 2 + evento para mostrar productos
 function displayProducts(productList) {
     shopContent.innerHTML = "";
 
-    if (productList.length === 0) {
+    if (productList.length === 0) {   // If
         noResults.style.display = "block";
         return;
     }
     noResults.style.display = "none";
 
-    productList.forEach((product) => {
-        const content = document.createElement("div");
+    productList.forEach((product) => {  // For each para recorrer el Array
+        let content = document.createElement("div");
         content.className = "cardProduct";
         content.innerHTML = `
             <img src="${product.img}">
             <h3>${product.name}</h3>
             <p class="size">${product.size}</p>
             <h3 class="price">€${product.price}</h3>
-            <button>Add to Cart</button>
         `;
         shopContent.append(content);
+
+        let addToCart = document.createElement("button");
+        addToCart.innerText = "Add to cart";
+        addToCart.className = "addCart";
+        
+        content.append(addToCart);
+        
+        addToCart.addEventListener("click", () => {   // Evento: Agregar al Carrito y Contar unidades / Metodos .some y .push
+                        
+            let currentCart = JSON.parse(localStorage.getItem("cart")) || [];
+
+            const count = currentCart.some((countUnit) => countUnit.id === product.id);
+
+            if(count){
+                currentCart.map((item) => {
+                    if(item.id === product.id) {
+                        item.units++;
+                    }
+                });
+            } else {
+                currentCart.push({
+                    id: product.id,
+                    img: product.img,
+                    name: product.name,
+                    price: product.price,
+                    units: product.units,
+                });
+            }
+
+            localStorage.setItem("cart", JSON.stringify(currentCart));
+            
+            console.log(currentCart);
+            countingCart();
+        });
     });
 };
 
-// Funcion de buscador de productos filtrando por nombre + evento
+// Funcion de buscador de productos filtrando por nombre y tipo
+
+//Funcion de orden superior 2 + evento
 
 function searchProduct() {
     const searchTerm = searchBar.value.toLowerCase();
@@ -48,7 +107,9 @@ function searchProduct() {
     displayProducts(filteredProducts)
 };
 
-searchBar.addEventListener("input", searchProduct); // Evento
+if (searchBar) {
+    searchBar.addEventListener("input", searchProduct); // Evento
+}
 
 getProducts();
 
